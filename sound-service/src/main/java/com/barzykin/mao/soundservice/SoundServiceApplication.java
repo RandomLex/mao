3fbf9974091f8b6c76d8880054505118bd716df9
Alexej_Barzykin <alexej.barzykin@wolterskluwer.com>
2024-10-20 00:59:44 +0400

    root and two subprojects

--- sound-service/src/main/java/com/barzykin/mao/soundservice/SoundServiceApplication.java
+++ sound-service/src/main/java/com/barzykin/mao/soundservice/SoundServiceApplication.java
@@ -1,4 +1,11 @@
 package com.barzykin.mao.soundservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SoundServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SoundServiceApplication.class, args);
+    }
 }
--- sound-service/src/test/java/com/barzykin/mao/soundservice/SoundServiceApplicationTest.java
+++ sound-service/src/test/java/com/barzykin/mao/soundservice/SoundServiceApplicationTest.java
@@ -1,4 +1,11 @@
 package com.barzykin.mao.soundservice;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class SoundServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
 }
